14a987279bc439aa9b79be4029e3cb43bfde2f81
Joshua Ayanlowo <joshuaayanlowo235@gmail.com>
2024-05-06 16:11:43 +0100

    Forms to CRUD and User account

--- blog/tests.py
+++ blog/tests.py
@@ -52,7 +52,32 @@ class BlogTests(TestCase):
         self.assertContains(response, 'Nice body content')
         self.assertTemplateUsed(response, 'post_detail.html')
         
-
-
-
-
+    def test_post_createview(self):
+        reponse = self.client.post(
+            reverse('post_new'),
+            {
+                'title': 'New Title',
+                'body': 'New Text',
+                'author': self.user.id,
+            },
+        )
+        self.assertEqual(reponse.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'New Title')
+        self.assertEqual(Post.objects.last().body, 'New Text')
+    
+    def test_post_updateview(self):
+        response = self.client.post(
+            reverse('post_edit', args='1'),
+            
+            {
+                'title': 'Updated title',
+                'body': 'Updated text',
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'Updated title')
+        self.assertEqual(Post.objects.last().body, 'Updated text')
+        
+    def test_post_deleteview(self):
+        respone = self.client.post(reverse('post_delete', args='1'))
+        self.assertEqual(respone.status_code, 302)
